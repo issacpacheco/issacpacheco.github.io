6f1b130071f450c20856188b475584cc16a88457
ISSAC LAPTOP <isaacpacheco.go@gmail.com>
2025-05-06 10:21:05 -0600

    Se agregan algunos cambios en la plataforma

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,5 +51,34 @@ $(document).ready(function () {
     light.style.opacity = '1';
   });
 
+  window.onload = function() {
+    // Para cada barra de progreso, animarla con un valor diferente
+    animateProgressBar('progress1', 95);
+    animateProgressBar('progress2', 90);
+    animateProgressBar('progress3', 90);
+    animateProgressBar('progress4', 95);
+    animateProgressBar('progress5', 90);
+    animateProgressBar('progress6', 85);
+    animateProgressBar('progress7', 95);
+    animateProgressBar('progress8', 75);
+};
+
+// Función que anima la barra de progreso según el id y el porcentaje
+function animateProgressBar(progressId, percentage) {
+    var progressBar = document.querySelector(`#${progressId} .progress-bar`);
+    var spanprogressBar = document.querySelector(`#span${progressId}`);
+    var width = 0;
+
+    var interval = setInterval(function() {
+        if (width >= percentage) {
+            clearInterval(interval);  // Detener la animación cuando llegue al porcentaje final
+        } else {
+            width++;
+            progressBar.style.width = width + '%';  // Actualizar el ancho de la barra
+            spanprogressBar.textContent = width + '%';
+        }
+    }, 30);  // Actualizar cada 30ms (puedes ajustar la velocidad)
+}
+
 });
 
